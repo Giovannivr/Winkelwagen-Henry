7e0a970c138abfee5e2e3a4f7a0c237e83beb024
Giovannivr <giovannivr@live.com>
2019-09-18 17:31:25 +0200

    Homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(ProductRepository $productRepository)
     {
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 }
+
